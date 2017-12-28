e3fcb48f8a79deb44f75ac1f4a12580b31c6b768
Joao Ribeiro <31766531+Loosemond@users.noreply.github.com>
2017-12-28 19:38:20 +0000

    can read many tweets and look for words in them
    Now you have to use a file to load your keys

--- TweepyMain.py
+++ TweepyMain.py
@@ -1,20 +1,27 @@
 import tweepy
 import random
 
-# Constants with the auth and consumer keys
-CONSUMER_KEY=''
-CONSUMER_SECRET=''
-OAUTH_TOKEN=''
-OAUTH_TOKEN_SECRET=''
+print(sys.version)
+print(" ")
+# file must contain keys by order and finish with a \n(enter)
+file = open("chave.txt","r")
+
+key=['null']*4
+i=0
+for line in file:
+    key[i]=line[0:-1]
+    i+=1
+file.close()
 
 #Creates a new Auth to Twitter API
-auth=tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth = tweepy.OAuthHandler(key[0], key[1])
 
 #Sets the tokens for the api access
-auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+auth.set_access_token(key[2], key[3])
 
 #Creates a new API with the auth
-api=tweepy.API(auth)
+api = tweepy.API(auth)
+
 
 # Gets all the tweets from the user
 #public_tweets=api.home_timeline()
@@ -24,3 +31,23 @@ api=tweepy.API(auth)
    # print(tweet.text)
 
 #api.retweet(666)
+
+#escreve os tweets dentro da range e da skip aos que nao exitem ou sao privados
+#tb procura pela palavra have !
+word='have'
+contador=0
+for i in range (2000,2005):
+    while True:
+        try:
+            var=api.get_status(i).text
+            print(var)
+            if word in var:
+                contador += 1
+            break
+        except:
+            print("----------Oops!------------ ")
+            break
+
+
+
+print("\nforam encontados {} {}".format(contador,word))
